9ffcc782610a531d8bb31e01007a8208236b6e96
Morteza Malvandi <malvandi.m@gmail.com>
2025-11-23 18:03:53 +0330

    Rename NotSupportedTypeException

--- src/main/kotlin/ir/bamap/blu/exception/NotSupportedTypeException.kt
+++ src/main/kotlin/ir/bamap/blu/exception/NotSupportedTypeException.kt
@@ -1,13 +1,13 @@
 package ir.bamap.blu.exception
 
-open class NotSupportedException(
-    val typeEntry: String,
+open class NotSupportedTypeException(
+    val type: String,
     placeId: String,
     message: String = "{blu.notSupported}",
     args: Map<String, Any?> = emptyMap(),
 ) : NotImplementedException(
     placeId, message,
-    mapOf("typeEntry" to typeEntry, "placeId" to placeId, *args.entries.map { it.toPair() }.toTypedArray())
+    mapOf("typeEntry" to type, "placeId" to placeId, *args.entries.map { it.toPair() }.toTypedArray())
 ) {
 
     constructor(type: Class<*>, placeId: String) : this(type.simpleName, placeId)
